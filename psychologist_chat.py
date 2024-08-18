9d4067e8e32cd8fd3748ecab4cba5846f18d2d63
wookjaelee <94896426+wookjaelee@users.noreply.github.com>
2024-08-18 18:59:57 +0900

    psychologist_chat.py

--- psychologist_chat.py
+++ psychologist_chat.py
@@ -7,7 +7,23 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.callbacks.base import BaseCallbackHandler
 
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_to(self, token:str, **kwargs)-> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+# 이전 대화 기록 출력
+def print_messages():    
+    if 'messages' in st.session_state and len(st.session_state['messages']) > 0 :
+        for chat_message in st.session_state['messages']:
+            st.chat_message(chat_message.role).write(chat_message.content)
+            
 st.set_page_config(page_title = 'Psychologist')
 st.title('Psychologist')
 
